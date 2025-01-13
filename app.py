4250da92fe9d2dd62fdf166d4aa9ac059e8e81e4
Linda Tir <fischweinen@gmail.com>
2025-01-13 11:18:51 -0500

    Adding pages I've been working on
    Not really done but just if you'd like my garbage

--- app.py
+++ app.py
@@ -20,5 +20,17 @@ def index():
 def login():
     return render_template("login.html")
 
+@app.route('/audit')
+def audit():
+    return render_template("audit.html")
+
+@app.route('/acctCreate')
+def acctCreate():
+    return render_template("acctCreate.html")
+
+@app.route('/settings')
+def settings():
+    return render_template("settings.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
